24f700e09f4c1881ca981f382ffa04e27b70c420
Peter Conerly <pconerly@gmail.com>
2023-06-26 18:48:01 -0700

    try fixing the table pathname, also save the artifact

--- .github/workflows/scripts/emscripten-build.sh
+++ .github/workflows/scripts/emscripten-build.sh
@@ -13,7 +13,7 @@ emmake make install prefix="$(pwd)/out-emscripten-install"
 
 #buildjs "16" "build-no-tables-utf16.js" &&
 #buildjs "16" "build-no-tables-wasm-utf16.js" "-s WASM=1" &&
-#buildjs "16" "build-tables-embeded-root-utf16.js" "--embed-files ./out-emscripten-install/share/liblouis/tables@/" &&
+buildjs "16" "build-tables-embeded-root-utf16.js" "--embed-files ./out-emscripten-install/share/liblouis/tables/" &&
 
 echo "[liblouis-js] configuring and making UTF-32 builds..." &&
 emconfigure ./configure --enable-ucs4 --disable-shared &&
@@ -22,6 +22,6 @@ emmake make &&
 echo "[liblouis-js] building UTF-32 with no tables..." &&
 buildjs "32" "build-no-tables-utf32.js" &&
 buildjs "32" "build-no-tables-wasm-utf32.js" "-s WASM=1" &&
-buildjs "32" "build-tables-embeded-root-utf32.js" "--embed-files ./out-emscripten-install/share/liblouis/tables@/" &&
+buildjs "32" "build-tables-embeded-root-utf32.js" "--embed-files ./out-emscripten-install/share/liblouis/tables/" &&
 
 echo "[liblouis-js] done building in docker image..."
